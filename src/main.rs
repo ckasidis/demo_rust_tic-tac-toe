56a4a8556ce7cc965501831f3a82228047d505cd
Kasidis Chantharojwong <me@ckasidis.com>
2022-07-18 22:24:34 +0700

    accept player names

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    let mut player_one = String::new();
+    let mut player_two = String::new();
+    println!("Enter Player 1");
+    stdin().read_line(&mut player_one).expect("a string");
+    println!("Enter Player 2");
+    stdin().read_line(&mut player_two).expect("a string");
+    println!("{} vs {}", player_one.trim(), player_two.trim());
 }
