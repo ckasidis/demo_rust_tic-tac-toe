e68f9beed4c1507903d003340db9d524ce300139
Kasidis Chantharojwong <me@ckasidis.com>
2022-07-19 20:07:49 +0700

    add print_board function

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,11 @@ fn main() {
     println!("Enter Player 2");
     stdin().read_line(&mut player_two).expect("a string");
     println!("{} vs {}", player_one.trim(), player_two.trim());
+
+    let board = ['-'; 9];
+    print_board(board);
+}
+
+ fn print_board(board: [char; 9]) {
+    println!("{} {} {}\n{} {} {}\n{} {} {}", board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]);
 }
