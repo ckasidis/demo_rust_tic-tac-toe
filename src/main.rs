c104936b342e179d62c48a202f49a2880d0f5e34
Kasidis Chantharojwong <me@ckasidis.com>
2022-07-20 17:49:29 +0700

    create check_winner and check_three functions

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,26 @@ fn main() {
         name: String::from(player_two.trim()),
         sign: 'o'
     };
-
     print_players(&player1, &player2);
 
-    let board = ['-'; 9];
-    print_board(board);
+    let board = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    
+    let mut round = 1;
+    let result = loop {
+        if round > 9 { break '-' };
+        print_board(board);
+        let Player {name: player_name, sign: player_sign} = if round%2 == 1 {&player1} else {&player2};
+        println!("{}'s turn ({})", player_name, player_sign);
+        if check_winner(board) == 'x' {break 'x'}
+        else if check_winner(board) == 'o' {break 'o'}
+        round += 1;
+    };
+
+    if result != '-' {
+        println!("The winner is {}!", result);
+    } else {
+        println!("It's a tie!")
+    }
 }
 
 struct Player {
@@ -30,9 +45,32 @@ struct Player {
 }
 
 fn print_players(player1: &Player, player2: &Player) {
-    println!("{} ({}) vs {} ({})" ,player1.name, player1.sign, player2.name, player2.sign)
+    println!("{} ({}) vs {} ({})" ,player1.name, player1.sign, player2.name, player2.sign);
 }
 
 fn print_board(board: [char; 9]) {
-    println!("{} {} {}\n{} {} {}\n{} {} {}", board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]);
+    println!("----------\n{} {} {}\n{} {} {}\n{} {} {}\n----------", 
+    board[0], board[1], board[2],
+    board[3], board[4], board[5],
+    board[6], board[7], board[8]);
+}
+
+fn check_winner(board: [char; 9]) -> char {
+    //horizontals
+    if check_three(board, 0, 1, 2) { board[0] }
+    else if check_three(board, 3, 4, 5) { board[3] }
+    else if check_three(board, 6, 7, 8) { board[6] }
+    //verticals
+    else if check_three(board, 0, 3, 6) { board[0] }
+    else if check_three(board, 1, 4, 7) { board[1] }
+    else if check_three(board, 2, 5, 8) { board[2] }
+    //diagonals
+    else if check_three(board, 0, 4, 8) { board[0] }
+    else if check_three(board, 2, 4, 6) { board[2] }
+    //no winner
+    else {'-'}
+}
+
+fn check_three(board: [char; 9], i1: usize, i2: usize, i3: usize) -> bool {
+    if board[i1] == board[i2] && board[i1] == board[i3] { true } else { false }
 }
